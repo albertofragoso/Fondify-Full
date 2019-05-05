60baabf3b5b339ae8a0cf0becf64df502f4fbab2
albertofragoso <hola@albertofragoso.com>
2019-05-04 23:33:07 -0500

    Dashboard user done.

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,33 @@
 const router = require('express').Router()
 const User   = require('../models/User')
 const Order  = require('../models/Order')
+const MenuUser = require('../models/MenuUser')
+const { isLogged, checkRole } = require('../helpers/middlewares')
 
-router.get('/', (req, res, next) => {
+router.get('/', isLogged, checkRole('user'), (req, res, next) => {
   const { _id } = req.user
-  Order.find({user: _id}).populate('user').populate('fonda').populate('menuUser')
+  Order.find({ user: _id }).populate('user').populate('fonda').populate('menuUser')
     .then(orders => {
-      console.log(orders)
-      res.render('user/dashboard', {orders})
+      res.render('user/user', { orders })
     })
     .catch(err => next(err))
 })
 
-router.get('/delete/:id', (req,res,next) => {
+router.post('/remove-menu', isLogged, checkRole('user'), (req, res, next) => {
+  const { order, menu } = req.body
+  Order.findByIdAndUpdate(order, { $pull : { menuUser: menu }},{ new: true })
+    .then(order => {
+      MenuUser.findByIdAndDelete(menu)
+        .then(() => res.redirect('/user'))
+        .catch(err => next(err))
+    })
+    .catch(err => next(err))
+})
+
+router.get('/reservation/:id/delete', isLogged, checkRole('user'), (req,res,next) => {
   const { id } = req.params
   Order.findByIdAndDelete(id)
-    .then(() => {
-      res.redirect('/')
-    })
+    .then(() => res.redirect('/user'))
     .catch(err => next(err))
 })
 
